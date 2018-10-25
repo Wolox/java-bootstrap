289db4f82f8bf371a570eb71a3cde5839302d201
Jime <jimena.rosello@wolox.com.ar>
2018-10-25 20:00:52 -0300

    First commit - Addition of a User class

---
+++ src/main/java/wolox/bootstrap/models/User.java
@@ -0,0 +1,61 @@
+package wolox.bootstrap.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(nullable = false)
+	private String username;
+
+	@Column
+	private String name;
+
+	@Column
+	private String password;
+
+	public User() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
