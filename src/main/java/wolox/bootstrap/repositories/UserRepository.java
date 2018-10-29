82c6aeda4c0f635a9d5a038f46749b9490864291
Jime <jimena.rosello@wolox.com.ar>
2018-10-29 09:00:52 -0300

    Correction in UserRepository

--- src/main/java/wolox/bootstrap/repositories/UserRepository.java
+++ src/main/java/wolox/bootstrap/repositories/UserRepository.java
@@ -2,10 +2,8 @@ package wolox.bootstrap.repositories;
 
 import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
 import wolox.bootstrap.models.User;
 
-@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
 	List<User> findByUsername(String username);
