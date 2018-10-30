61b89e5b891331f81bfd5c6533ad78a54c1bd676
NicolasG <nicolas.gianni@wolox.com.ar>
2018-10-30 18:16:50 -0300

    Internationalization-String

--- src/main/java/wolox/bootstrap/BootstrapApplication.java
+++ src/main/java/wolox/bootstrap/BootstrapApplication.java
@@ -1,12 +1,30 @@
 package wolox.bootstrap;
 
+import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class BootstrapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapApplication.class, args);
+	}
+
+	/*Con el método localeResolver le indicamos a Spring que vamos a definir el idioma de las respuestas del webservice,
+	utilizando cabeceras en nuestros requests al mismo.
+	Como idioma predeterminado utilizaremos el inglés americano (Locale.US)*/
+
+	@Bean
+	public AcceptHeaderLocaleResolver localeResolver(){
+
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+
+		localeResolver.setDefaultLocale(Locale.US);
+
+		return localeResolver;
+
 	}
 }
