488d84558d1d06977e34cb9e99265d414f097118
naipes0787 <leandro.costa@wolox.com.ar>
2020-01-31 13:17:24 -0300

    Changing PasswordValidator attributes visibility

--- src/main/java/wolox/bootstrap/miscelaneous/PasswordValidator.java
+++ src/main/java/wolox/bootstrap/miscelaneous/PasswordValidator.java
@@ -2,8 +2,8 @@ package wolox.bootstrap.miscelaneous;
 
 public class PasswordValidator {
 
-	public static final int LENGTH = 8;
-	public static final String REGEX = ".{0,}[^a-zA-Z\\d\\s:].{0,}";
+	private static final int LENGTH = 8;
+	private static final String REGEX = ".{0,}[^a-zA-Z\\d\\s:].{0,}";
 
 	public static boolean passwordIsValid(String password) {
 		return (password.length() >= LENGTH && password.matches(REGEX));
