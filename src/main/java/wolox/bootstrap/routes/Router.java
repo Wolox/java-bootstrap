0efc0f66e6c189f5bf1012b8b498d0a8794ac36f
Jimenarosello <jimena.rosello@gmail.com>
2020-02-21 20:15:04 -0300

    Add service call (without response processing)

--- src/main/java/wolox/bootstrap/routes/Router.java
+++ src/main/java/wolox/bootstrap/routes/Router.java
@@ -5,6 +5,7 @@ import static org.apache.camel.builder.PredicateBuilder.isNull;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import wolox.bootstrap.exceptions.MissingParameterException;
 import wolox.bootstrap.processors.MomentOfDayProcessor;
@@ -15,6 +16,11 @@ import wolox.bootstrap.utils.Constants;
 public class Router extends RouteBuilder {
 
     private static final String NAME_TAG = "name";
+    private static final String WEATHER_SERVICE_URL =
+            "http://api.weatherstack.com/current?bridgeEndpoint=true&access_key=%s&query=%s&units=m";
+
+    @Value("${api.key}")
+    private static String apiKey;
 
     @Autowired
     private MomentOfDayProcessor momentOfDayProcessor;
@@ -29,6 +35,7 @@ public class Router extends RouteBuilder {
         rest(CamelConstants.GREETING_ENDPOINT)
             .get(CamelConstants.HELLO_ENDPOINT).route().to(CamelConstants.HELLO_ROUTE).endRest()
             .get(CamelConstants.BYE_ENDPOINT).route().to(CamelConstants.BYE_ROUTE).endRest()
+            .get(CamelConstants.WEATHER_ENDPOINT).route().to(CamelConstants.WEATHER_ROUTE).endRest()
         ;
 
 
@@ -49,6 +56,11 @@ public class Router extends RouteBuilder {
 
         from(CamelConstants.BYE_ROUTE)
             .setBody(simple(Constants.BYE_MESSAGE))
+        ;
+
+        from(CamelConstants.WEATHER_ROUTE)
+            .setProperty("city", header("city"))
+            .to(String.format(WEATHER_SERVICE_URL, apiKey, "buenos aires"))
         ;
 
     }
--- src/main/java/wolox/bootstrap/utils/CamelConstants.java
+++ src/main/java/wolox/bootstrap/utils/CamelConstants.java
@@ -5,9 +5,11 @@ public class CamelConstants {
 	public static final String HELLO_ENDPOINT = "hello";
 	public static final String BYE_ENDPOINT = "bye";
 	public static final String GREETING_ENDPOINT = "greeting";
+	public static final String WEATHER_ENDPOINT = "weather";
 
 	public static final String HELLO_ROUTE = "direct:say-hi";
 	public static final String BYE_ROUTE = "direct:say-bye";
+	public static final String WEATHER_ROUTE = "direct:get-weather-advice";
 
 	/**
 	 * Private constructor to avoid instantiation.
