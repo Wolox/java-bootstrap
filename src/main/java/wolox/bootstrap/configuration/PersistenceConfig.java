af5f58fb5e0173250a142329b806f0df1fe1f9dd
rodrigofranc <rodrigo.francou@wolox.com.ar>
2020-03-24 20:35:41 -0300

    add auditable class and config

---
+++ src/main/java/wolox/bootstrap/configuration/PersistenceConfig.java
@@ -0,0 +1,8 @@
+package wolox.bootstrap.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class PersistenceConfig { }
---
+++ src/main/java/wolox/bootstrap/models/Auditable.java
@@ -0,0 +1,24 @@
+package wolox.bootstrap.models;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+	@CreatedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date creationDate;
+
+	@LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date lastModifiedDate;
+
+}
