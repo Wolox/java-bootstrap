a0617cb185b059097d70e7a3eb5abcc935bde026
Marcelo Guzman <marcegl91@gmail.com>
2020-04-14 13:29:31 -0300

    Add Spock in bootstrap

---
+++ src/test/groovy/wolox/bootstrap/models/EmptyStackSpec.groovy
@@ -0,0 +1,30 @@
+package wolox.bootstrap.models
+
+import spock.lang.Specification
+
+class EmptyStackSpec extends Specification {
+    def stack = new Stack()
+
+    def "size"() {
+        expect: stack.size() == 0
+    }
+
+    def "pop"() {
+        when: stack.pop()
+        then: thrown(EmptyStackException)
+    }
+
+    def "peek"() {
+        when: stack.peek()
+        then: thrown(EmptyStackException)
+    }
+
+    def "push"() {
+        when:
+        stack.push("elem")
+
+        then:
+        stack.size() == old(stack.size()) + 12
+        stack.peek() == "elem"
+    }
+}
