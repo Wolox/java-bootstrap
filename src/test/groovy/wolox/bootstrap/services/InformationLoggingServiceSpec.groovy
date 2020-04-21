fe2ea64900225e7afb6a6b71b4c850ec108adef0
Marcelo Guzman <marcegl91@gmail.com>
2020-04-21 12:17:09 -0300

    Change logRepository to use Spock mock annotation

--- src/test/groovy/wolox/bootstrap/services/InformationLoggingServiceSpec.groovy
+++ src/test/groovy/wolox/bootstrap/services/InformationLoggingServiceSpec.groovy
@@ -1,8 +1,8 @@
 package wolox.bootstrap.services
 
+import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.ComponentScan
 import spock.lang.Specification
 import wolox.bootstrap.repositories.LogRepository
@@ -10,8 +10,9 @@ import wolox.bootstrap.repositories.LogRepository
 @DataJpaTest
 @ComponentScan
 class InformationLoggingServiceSpec extends Specification {
-    @MockBean
-    LogRepository logRepository
+
+    @SpringBean
+    LogRepository logRepository = Mock()
 
     @Autowired
     InformationLoggingService service
