ddc1ba54a00984e809e7dd9f73385f5f1f5812eb
Maximiliano Paz <maximilianopaz@MacBook-Pro-de-Wolox.local>
2020-04-30 17:23:05 -0300

    Fix Tests and add new GCs options to README

--- src/test/java/wolox/bootstrap/BootstrapApplicationTests.java
+++ src/test/java/wolox/bootstrap/BootstrapApplicationTests.java
@@ -3,10 +3,14 @@ package wolox.bootstrap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@PropertySource(value = "classpath:application.properties")
 public class BootstrapApplicationTests {
 
 	@Test
