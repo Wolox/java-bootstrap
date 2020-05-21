eb04983a3a30debfc826ecbd5710e89cc80ba7a7
Jimenarosello <jimena.rosello@gmail.com>
2020-05-21 16:07:33 -0300

    Add swagger documentation

--- src/main/java/wolox/bootstrap/controllers/RoleController.java
+++ src/main/java/wolox/bootstrap/controllers/RoleController.java
@@ -1,8 +1,12 @@
 package wolox.bootstrap.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import java.net.URI;
 import java.util.Optional;
 import javax.management.relation.RoleNotFoundException;
-
 import lombok.extern.slf4j.Slf4j;
 import org.postgresql.shaded.com.ongres.scram.common.util.Preconditions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,11 +31,10 @@ import wolox.bootstrap.repositories.RoleRepository;
 import wolox.bootstrap.repositories.UserRepository;
 import wolox.bootstrap.utils.Constants;
 
-import java.net.URI;
-
 @Slf4j
 @RestController
 @RequestMapping(value = "/api/roles")
+@Api
 public class RoleController {
 
     @Autowired
@@ -43,24 +46,34 @@ public class RoleController {
     @Autowired
     private MessageSource messageSource;
 
+    @ApiOperation(value = "Given role information, it creates a role")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully created role")
+    })
     @PostMapping
     public ResponseEntity<Role> create(@RequestBody RoleDto roleDto) throws RoleNotFoundException {
         Preconditions.checkArgument(!roleRepository.findByName(roleDto.getName()).isPresent(),
-            messageSource.getMessage(Constants.MSG_CODE_EXISTING_ROLE, null, LocaleContextHolder
-                .getLocale()));
+                messageSource.getMessage(Constants.MSG_CODE_EXISTING_ROLE, null, LocaleContextHolder
+                        .getLocale()));
         final Role role = new Role(roleDto.getName());
         log.info("Received role:" + role);
         log.info("Saving role ");
         roleRepository.save(role);
         final URI uri = ControllerLinkBuilder
-                .linkTo(ControllerLinkBuilder.methodOn(RoleController.class).findById(role.getId())).toUri();
+                .linkTo(ControllerLinkBuilder.methodOn(RoleController.class).findById(role.getId()))
+                .toUri();
         return ResponseEntity.created(uri).body(role);
     }
 
+    @ApiOperation(value = "Given a role name and/or username, it returns a role")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Found role")
+
+    })
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping
     public ResponseEntity<Iterable<Role>> find(@RequestParam(defaultValue = "") final String name,
-                                               @RequestParam(defaultValue = "") final String username) {
+            @RequestParam(defaultValue = "") final String username) {
         final Optional<User> userOptional = userRepository.findByUsername(username);
         final Iterable<Role> roles = userOptional.isPresent() ? roleRepository
                 .findByNameContainingAndUsersIsInAllIgnoreCase(name, userOptional.get())
@@ -68,30 +81,50 @@ public class RoleController {
         return ResponseEntity.ok(roles);
     }
 
+    @ApiOperation(value = "Given an id, it returns a role")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Found role"),
+            @ApiResponse(code = 404, message = "Role id not found")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Role> findById(@PathVariable final int id) throws RoleNotFoundException {
-        return ResponseEntity.ok(roleRepository.findById(id).orElseThrow(() -> new RoleNotFoundException(
-            messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_ROLE, null, LocaleContextHolder
-                .getLocale()))));
+        return ResponseEntity
+                .ok(roleRepository.findById(id).orElseThrow(() -> new RoleNotFoundException(
+                        messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_ROLE, null,
+                                LocaleContextHolder
+                                        .getLocale()))));
     }
 
+    @ApiOperation(value = "Given an id and the new role information, it updates the role")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Updated role"),
+            @ApiResponse(code = 404, message = "Role id not found")
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<Role> update(@RequestBody final RoleDto roleDto, @PathVariable final int id)
-        throws RoleNotFoundException {
+    public ResponseEntity<Role> update(@RequestBody final RoleDto roleDto,
+            @PathVariable final int id)
+            throws RoleNotFoundException {
         final Role role = roleRepository.findById(id).orElseThrow(() -> new RoleNotFoundException(
-            messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_ROLE, null, LocaleContextHolder
-                .getLocale())));
+                messageSource
+                        .getMessage(Constants.MSG_CODE_NOT_EXISTING_ROLE, null, LocaleContextHolder
+                                .getLocale())));
         this.setRoleFromRoleDto(role, roleDto);
         roleRepository.save(role);
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value = "Given an id, it deletes a role")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Found role"),
+            @ApiResponse(code = 404, message = "Role id not found")
+    })
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable final int id) throws RoleNotFoundException {
         final Role role = roleRepository.findById(id).orElseThrow(() -> new RoleNotFoundException(
-            messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_ROLE, null, LocaleContextHolder
-                .getLocale())));
+                messageSource
+                        .getMessage(Constants.MSG_CODE_NOT_EXISTING_ROLE, null, LocaleContextHolder
+                                .getLocale())));
         for (final User user : role.getUsers()) {
             user.getRoles().remove(role);
         }
@@ -101,6 +134,7 @@ public class RoleController {
 
     /**
      * Set the name (Using uppercase) to the {@link Role} from the {@link RoleDto}
+     *
      * @param role The {@link Role} to be updated
      * @param roleDto The {@link RoleDto} from where the information will be obtained
      */
--- src/main/java/wolox/bootstrap/controllers/UserController.java
+++ src/main/java/wolox/bootstrap/controllers/UserController.java
@@ -1,5 +1,11 @@
 package wolox.bootstrap.controllers;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import java.net.URI;
+import java.util.Optional;
+import javax.management.relation.RoleNotFoundException;
 import org.postgresql.shaded.com.ongres.scram.common.util.Preconditions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -28,10 +34,6 @@ import wolox.bootstrap.repositories.UserRepository;
 import wolox.bootstrap.utils.Constants;
 import wolox.bootstrap.utils.PasswordValidator;
 
-import javax.management.relation.RoleNotFoundException;
-import java.net.URI;
-import java.util.Optional;
-
 @RestController
 @RequestMapping(value = "/api/users")
 public class UserController {
@@ -48,93 +50,145 @@ public class UserController {
     @Autowired
     private MessageSource messageSource;
 
+    @ApiOperation(value = "Given user information, it creates a user")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully created user"),
+            @ApiResponse(code = 400, message = "The password is not valid"),
+            @ApiResponse(code = 400, message = "The user already exists")
+    })
     @PostMapping
     public ResponseEntity<User> create(@RequestBody final UserRequestDto userRequestDto) {
         Preconditions
-            .checkArgument(!userRepository.findByUsername(userRequestDto.getUsername()).isPresent(),
-                messageSource.getMessage(Constants.MSG_CODE_EXISTING_USER, null, LocaleContextHolder
-                    .getLocale()));
+                .checkArgument(
+                        !userRepository.findByUsername(userRequestDto.getUsername()).isPresent(),
+                        messageSource.getMessage(Constants.MSG_CODE_EXISTING_USER, null,
+                                LocaleContextHolder
+                                        .getLocale()));
         final String password = userRequestDto.getPassword();
         Preconditions
-            .checkArgument(PasswordValidator.passwordIsValid(password),
-                messageSource.getMessage(Constants.MSG_CODE_INVALID_PASSWORD, null, LocaleContextHolder
-                    .getLocale()));
-        final User user = new User(userRequestDto.getUsername(), userRequestDto.getName(), userRequestDto.getPassword());
+                .checkArgument(PasswordValidator.passwordIsValid(password),
+                        messageSource.getMessage(Constants.MSG_CODE_INVALID_PASSWORD, null,
+                                LocaleContextHolder
+                                        .getLocale()));
+        final User user = new User(userRequestDto.getUsername(), userRequestDto.getName(),
+                userRequestDto.getPassword());
         user.setPassword(passwordEncoder.encode(password));
         userRepository.save(user);
         final URI uri = ControllerLinkBuilder
                 .linkTo(ControllerLinkBuilder.methodOn(UserController.class)
-                        .find("",user.getUsername(),"")).toUri();
+                        .find("", user.getUsername(), "")).toUri();
         return ResponseEntity.created(uri).body(user);
     }
 
+    @ApiOperation(value = "Given a user name/username/roleName, it finds a user")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "User found"),
+            @ApiResponse(code = 404, message = "User not found")
+    })
     @GetMapping
     public ResponseEntity<Iterable<User>> find(@RequestParam(defaultValue = "") final String name,
-        @RequestParam(defaultValue = "") final String username,
-        @RequestParam(defaultValue = "") final String roleName) {
+            @RequestParam(defaultValue = "") final String username,
+            @RequestParam(defaultValue = "") final String roleName) {
         final Optional<Role> roleOpt = roleRepository.findByName(roleName);
         Iterable<User> users = roleOpt.isPresent() ? userRepository
-            .findByNameContainingAndUsernameContainingAndRolesIsInAllIgnoreCase(name,
-                username, roleOpt.get())
-            : userRepository
-                .findByNameContainingAndUsernameContainingAllIgnoreCase(name, username);
+                .findByNameContainingAndUsernameContainingAndRolesIsInAllIgnoreCase(name,
+                        username, roleOpt.get())
+                : userRepository
+                        .findByNameContainingAndUsernameContainingAllIgnoreCase(name, username);
         return ResponseEntity.ok(users);
     }
 
+    @ApiOperation(value = "Given a user id and its new information, it updates the user")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully created user"),
+            @ApiResponse(code = 400, message = "The password is not valid"),
+            @ApiResponse(code = 400, message = "The user already exists")
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<User> update(@RequestBody final UserRequestDto userRequestDto, @PathVariable final int id) {
+    public ResponseEntity<User> update(@RequestBody final UserRequestDto userRequestDto,
+            @PathVariable final int id) {
         final User user = userRepository.findById(id)
-            .orElseThrow(() -> new UsernameNotFoundException(
-                messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null, LocaleContextHolder
-                    .getLocale())));
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null,
+                                LocaleContextHolder
+                                        .getLocale())));
         this.setUserFromUserRequestDto(user, userRequestDto);
         userRepository.save(user);
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value = "Given a new password and an id, it updates the password of the user "
+            + "with the provided id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated user"),
+            @ApiResponse(code = 400, message = "The new password is not valid"),
+            @ApiResponse(code = 400, message = "The provided password does not match the user's old password"),
+            @ApiResponse(code = 404, message = "The user does not exist")
+    })
     @PreAuthorize("hasRole('ROLE_USER')")
     @PutMapping("/{id}/updatePassword")
     public ResponseEntity<User> updatePassword(@PathVariable final int id,
-        @RequestBody final PasswordModificationDto passwordModificationDto) throws RoleNotFoundException {
+            @RequestBody final PasswordModificationDto passwordModificationDto)
+            throws RoleNotFoundException {
         final User user = userRepository.findById(id)
-            .orElseThrow(() -> new UsernameNotFoundException(
-                messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null, LocaleContextHolder
-                    .getLocale())));
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null,
+                                LocaleContextHolder
+                                        .getLocale())));
         boolean equal = passwordEncoder
-            .matches(passwordModificationDto.getOldPassword(), user.getPassword());
+                .matches(passwordModificationDto.getOldPassword(), user.getPassword());
 
         final String newPassword = passwordModificationDto.getNewPassword();
         Preconditions
-            .checkArgument(equal,
-                messageSource.getMessage(Constants.MSG_CODE_WRONG_PASSWORD, null, LocaleContextHolder
-                    .getLocale()));
+                .checkArgument(equal,
+                        messageSource.getMessage(Constants.MSG_CODE_WRONG_PASSWORD, null,
+                                LocaleContextHolder
+                                        .getLocale()));
         user.setPassword(passwordEncoder.encode(newPassword));
         userRepository.save(user);
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value = "Given a role and an id, it adds the role to the user with "
+            + "the provided id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated roles"),
+            @ApiResponse(code = 404, message = "The user does not exist")
+    })
     @PutMapping("/{id}/addRole")
-    public ResponseEntity<User> update(@RequestBody final RoleDto roleDto, @PathVariable final int id) {
+    public ResponseEntity<User> update(@RequestBody final RoleDto roleDto,
+            @PathVariable final int id) {
         final User user = userRepository.findById(id)
-            .orElseThrow(() -> new UsernameNotFoundException(
-                messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null, LocaleContextHolder
-                    .getLocale())));
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null,
+                                LocaleContextHolder
+                                        .getLocale())));
         final Role role = new Role(roleDto.getName());
         user.addToRole(role);
         userRepository.save(user);
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value = "Given a role and an id, it removes the role from the user with "
+            + "the provided id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated roles"),
+            @ApiResponse(code = 404, message = "The user does not exist"),
+            @ApiResponse(code = 404, message = "The role does not exist")
+    })
     @PutMapping("/{id}/removeRole")
-    public ResponseEntity<User> updateRemove(@RequestBody final RoleDto roleDto, @PathVariable final int id) {
+    public ResponseEntity<User> updateRemove(@RequestBody final RoleDto roleDto,
+            @PathVariable final int id) {
         final User user = userRepository.findById(id)
-            .orElseThrow(() -> new UsernameNotFoundException(
-                messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null, LocaleContextHolder
-                    .getLocale())));
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null,
+                                LocaleContextHolder
+                                        .getLocale())));
         final Role role = roleRepository.findByName(roleDto.getName())
-            .orElseThrow(() -> new RuntimeException(
-                messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_ROLE, null, LocaleContextHolder
-                    .getLocale())));
+                .orElseThrow(() -> new RuntimeException(
+                        messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_ROLE, null,
+                                LocaleContextHolder
+                                        .getLocale())));
 
         user.removeRole(role);
         roleRepository.save(role);
@@ -142,18 +196,25 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value = "Given an id, it deletes the user with the provided id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Successfully deleted user"),
+            @ApiResponse(code = 404, message = "The user does not exist")
+    })
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable final int id) throws UsernameNotFoundException {
         final User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException(
-            messageSource.getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null, LocaleContextHolder
-                .getLocale())));
+                messageSource
+                        .getMessage(Constants.MSG_CODE_NOT_EXISTING_USER, null, LocaleContextHolder
+                                .getLocale())));
         userRepository.delete(user);
         return ResponseEntity.noContent().build();
     }
 
     /**
      * Set the name and the username to the {@link User} from the {@link UserRequestDto}
+     *
      * @param user The {@link User} to be updated
      * @param userRequestDto The {@link UserRequestDto} from where the information will be obtained
      */
