b32dacd72e387705163bb2e0c1a4f9ac6d696ac3
Jimenarosello <jimena.rosello@gmail.com>
2020-05-21 14:11:16 -0300

    Revert last commit

--- src/test/java/wolox/bootstrap/BootstrapApplicationTests.java
+++ src/test/java/wolox/bootstrap/BootstrapApplicationTests.java
@@ -3,10 +3,12 @@ package wolox.bootstrap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@PropertySource(value = "classpath:application.properties")
 public class BootstrapApplicationTests {
 
 	@Test
